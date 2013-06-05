3a52fa22492dc7dda0a3de89a0c2540535467bce
Tom Najdek <tom@doppnet.com>
2013-06-05 21:29:53 +0100

    simple mocha should be enough

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -12,14 +12,22 @@ module.exports = function(grunt) {
 				dest: 'build/<%= pkg.name %>.min.js'
 			}
 		},
-		mocha: {
-			index: ['specs/index.html']
-		},
+		simplemocha: {
+			options: {
+				globals: ['should'],
+				timeout: 3000,
+				ignoreLeaks: false,
+				grep: '*-test',
+				ui: 'bdd',
+				reporter: 'tap'
+			},
+			all: { src: ['test/*.js'] }
+		}
 	});
 
 	// Load the plugin that provides the "uglify" task. 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-mocha');
+	grunt.loadNpmTasks('grunt-simple-mocha');
 
 	// Default task(s).
 	grunt.registerTask('default', ['uglify']);
