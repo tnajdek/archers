e7dd8a4be2589ff4515eb043f8785c3353edf9d9
Tom Najdek <tom@doppnet.com>
2013-06-12 02:48:35 +0100

    Preparing front-end for end-to-end communication

--- frontend/src/archers.js
+++ frontend/src/archers.js
@@ -7,6 +7,13 @@
 //    }
 // };
 
-requirejs(['messaging'], function (messaging) {
-	// console.log(messaging);
+requirejs(['messaging'], function (Messaging) {
+	var ws = new WebSocket("ws://localhost:9000");
+	ws.binaryType = 'arraybuffer'
+	ws.onopen = function() {
+		ws.onmessage = function(e) {
+		// console.log(Messaging.fromBuffer(e.data));
+		console.log(e.data);
+		}
+	};
 });
