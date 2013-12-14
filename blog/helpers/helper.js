3b68984351299a0637471b9e22301f4d6b31a44c
Tom Najdek <tomek@potatolondon.com>
2013-12-14 04:10:12 +0000

    Blog: WIP: ordering of the posts on the home page On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   blog/helpers/helper.js
    	modified:   blog/package.json
    	modified:   blog/src/pages/index.hbs
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   backend/settings.py
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	.DS_Store
    	blog/.DS_Store
    	blog/.grunt/
    	frontend/.DS_Store
    	frontend/css/
    	frontend/public/.DS_Store

--- blog/helpers/helper.js
+++ blog/helpers/helper.js
@@ -1,8 +1,9 @@
 module.exports.register = function (Handlebars, options) {
+	var grunt = require('grunt'),
+			_ = require('lodash');
+
 	Handlebars.registerHelper('nextArticle', function (pages, cur, options) {
-		var grunt = require('grunt'),
-			_ = require('lodash'),
-			i,sorted, curPage, curIndex, nextIndex;
+		var i,sorted, curPage, curIndex, nextIndex;
 
 		sorted = _.sortBy(pages, function(item) {
 			return item.data.date;
@@ -18,5 +19,32 @@ module.exports.register = function (Handlebars, options) {
 		if(sorted[nextIndex]) {
 			return options.fn(sorted[nextIndex]);
 		}
+	});
+
+	Handlebars.registerHelper('getPosts', function(src, options) {
+		var yfm = require('assemble-yaml'),
+			glob = require('globule').
+			files, data;
+
+		options = options || {
+			fromFile: true
+		};
+
+		grunt.log.writeln(glob);
+		grunt.log.writeln('meh---------------');
+
+		files = glob.find(src);
+
+		data = files.map(function (path) {
+			return yfm.extract(path, options).context;
+		});
+
+		data = _.sortBy(data, function(item) {
+			return item.date;
+		});
+
+		return data.map(function (obj) {
+			return options.fn(obj);
+		}).join('');
 	});
 };
